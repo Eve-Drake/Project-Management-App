c7206ee49c89bc3ead3c8d2e48d3fb6f0e7ac070
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-08 15:03:22 +0100

    Start of implementing state management

---
+++ src/features/categories/state/projectContext.tsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+import { Task } from "../../tasks/TaskInterface";
+const ProjectContext = createContext();
+
+const ProjectProvider = ({children}) =>{
+    const [tasks, setTasks] = useState<Task[]>([{task:'Example Main Task', id: 0, category: 'Main'}, {task:'Example Secondary Task', id: 1, category: 'Secondary'}])
+    const [categories, setCategories] =  useState<string[]>(['Main', 'Secondary'])
+    const [activeCategory, setActiveCategory] = useState<string>('Main');
+    const [activeTaskArray, setActiveTaskArray] = useState<Task[]>([])
+
+
+return (
+    <ProjectContext.Provider value={{tasks, setTasks, categories, setCategories, activeCategory, setActiveCategory, activeTaskArray, setActiveTaskArray }}>
+        {children}
+    </ProjectContext.Provider>
+)
+}
