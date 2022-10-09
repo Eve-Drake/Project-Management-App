69b247682cd05015fa087f510ec00facda7be41c
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-09 13:02:23 +0100

    context api

--- src/features/categories/state/projectContext.tsx
+++ src/features/categories/state/projectContext.tsx
@@ -1,8 +1,8 @@
 import React, { createContext, useState } from "react";
 import { Task } from "../../tasks/TaskInterface";
-const ProjectContext = createContext();
+const ProjectContext = createContext<Task[] | string | null | string[]>(null);
 
-const ProjectProvider = ({children}) =>{
+const ProjectProvider = ({ children }) => {
     const [tasks, setTasks] = useState<Task[]>([{task:'Example Main Task', id: 0, category: 'Main'}, {task:'Example Secondary Task', id: 1, category: 'Secondary'}])
     const [categories, setCategories] =  useState<string[]>(['Main', 'Secondary'])
     const [activeCategory, setActiveCategory] = useState<string>('Main');
