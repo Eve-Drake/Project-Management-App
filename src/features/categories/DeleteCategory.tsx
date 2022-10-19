a8767ad2dac7444c9f80e834e1091cec563d7966
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-19 12:32:02 +0100

    Saving main category

--- src/features/categories/DeleteCategory.tsx
+++ src/features/categories/DeleteCategory.tsx
@@ -1,14 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import ActiveCategoryContext, { ActiveCategoryContexInterface } from '../state/ActiveCategoryContext';
 import CategoryContext, { CategoryContexInterface } from '../state/CategoryContext';
+import { BiX } from "react-icons/bi";
 
 interface Props {
-  category: string
+  category: string,
+  setAlert: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const DeleteCategory = ({category} : Props) => {
+const DeleteCategory = ({category, setAlert} : Props) => {
   const { deleteCategory } = useContext(CategoryContext) as CategoryContexInterface
+  const {activeCategory, selectCategory} =  useContext(ActiveCategoryContext) as ActiveCategoryContexInterface
+
+
+  const handleDelete =()=>{
+    setAlert(false)
+    if (category === 'Main'){
+      setAlert(true)
+      return
+    }
+    deleteCategory(category)
+    if (activeCategory === category){
+      selectCategory('Main')
+    }
+
+  }
+
   return (
-    <button onClick={()=>deleteCategory(category)}>X</button>
+    <button onClick={handleDelete}><BiX /></button>
   )
 }
 
--- src/features/categories/DisplayCategories.tsx
+++ src/features/categories/DisplayCategories.tsx
@@ -1,22 +1,26 @@
 import DeleteCategory from './DeleteCategory'
 import CategoryContext, { CategoryContexInterface } from '../state/CategoryContext';
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import ActiveCategoryContext, { ActiveCategoryContexInterface } from '../state/ActiveCategoryContext';
 
 const DisplayCategories = () => {
   const {categories} = useContext(CategoryContext) as CategoryContexInterface
   const {selectCategory} = useContext(ActiveCategoryContext) as ActiveCategoryContexInterface
 
+  const [alert, setAlert] = useState(false)
+
   return (
     <div className='overflow-y-auto h-5/6 '>
-      {categories.map((category)=>(
+     <p className={alert? 'block' : 'hidden'}>Cannot Delete Main Category</p> 
+     {categories.map((category)=>(
         <div key={category}>
           <button key={category} onClick={()=>selectCategory(category)} className='p-2 m-2 border rounded cursor-pointer w-5/6'> 
             {category}
           </button>
-          <DeleteCategory category={category}/>
+          <DeleteCategory category={category} setAlert={setAlert}/>
         </div>
       ))}
+    
     </div>
   )
 }
