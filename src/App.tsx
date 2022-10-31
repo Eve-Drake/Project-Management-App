e1ee017854186081f276bc22aa01e4f17e738801
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-31 15:42:14 +0000

    Adding Toggle Checkbox (Before styling)

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,7 @@ function App() {
   const {tasks} =  useContext(TaskContext) as TaskContexInterface
   const {categories} = useContext(CategoryContext) as CategoryContexInterface
   const {activeCategory} = useContext(ActiveCategoryContext) as ActiveCategoryContexInterface
-  const {darkMode} = useContext(LightDarkModeContext) as LightDarkModeInterface
-
+  const {darkMode, switchDarkMode} = useContext(LightDarkModeContext) as LightDarkModeInterface
 
   const [activeTaskArray, setActiveTaskArray] = useState<Task[]>([])
 
@@ -30,7 +29,7 @@ function App() {
 
     <div className="grid grid-cols-1 md:grid-cols-4">
       <div className="grid col-span-1 h-32 md:h-screen border-2 md:border-r-black ">
-
+        <input type='checkbox' onClick={switchDarkMode}/>
         <DisplayCategories />
         <AddCategory  />
       </div>
